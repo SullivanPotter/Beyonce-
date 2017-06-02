05e2145f1b63a2707e339002d7b5fdc8d2adbb6f
SullivanPotter <mih.sullivan22@gmail.com>
2017-06-02 14:26:04 -0300

    upload

---
+++ js/myplaylist.js
@@ -0,0 +1,81 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01 Crazy In Love.m4a',
+        title:'Crazy In Love',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:56',
+        cover:'mix/dangerously-in-love.jpg',
+    },
+
+    {
+        mp3:'mix/09 Irreplaceable.m4a',
+        title:'Irreplaceable',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:47',
+        cover:'mix/bday.jpg',
+    },
+    
+    {
+        mp3:'mix/01 Single Ladies (Put a Ring On It).m4a',
+        title:'Single Ladies (Put a Ring On It)',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:12',
+        cover:'mix/i am sasha fierce.jpg',
+    },
+
+    {
+        mp3:'mix/11 Run The World (Girls).m4a',
+        title:'Run The World (Girls)',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:58',
+        cover:'mix/4.jpg',
+    },
+    
+    {
+        mp3:'mix/01 Pretty Hurts.m4a',
+        title:'Pretty Hurts',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:58',
+        cover:'mix/beyonce.jpg',
+    },
+    
+    {
+        mp3:'mix/12 Formation.m4a',
+        title:'Formation',
+        artist:'Beyonce',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:26',
+        cover:'mix/lemonade.jpg',
+    }
+
+    
+    
+    
+];
+
